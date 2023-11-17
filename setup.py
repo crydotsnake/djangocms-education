cfcc14191bf4da68045b9c81c7ff50dd6a87bbab
Simon Krull <simonkrull@mailbox.org>
2023-11-17 18:20:04 +0100

    TASK: update package requirements and maintainer email

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from djangocms_education import __version__
 
 
 REQUIREMENTS = [
-    'django-cms>=3.7',
-    'djangocms-text-ckeditor>=3.1.0',
+    'django-cms>=3.11',
+    'djangocms-text-ckeditor>=5.1.4',
     'python-dateutil>=2.8.2',
 ]
 
@@ -14,9 +14,9 @@ setup(
     name='djangocms-education',
     version=__version__,
     author='Simon Krull',
-    author_email='simon@django-cms.org',
+    author_email='simonkrull@mailbox.org',
     maintainer='Simon Krull',
-    maintainer_email='simon@django-cms.org',
+    maintainer_email='simonkrull@mailbox.org',
     url='https://github.com/crydotsnake/djangocms-education',
     license='BSD-3-Clause',
     description='Adds a Plugin for embedding your Resume to your website.',
